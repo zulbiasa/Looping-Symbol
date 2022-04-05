a2634c25a99851de8fe93cbe65496082f6adab39
USER <USER@ZULBIASA02>
2022-04-05 11:45:32 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim y As Integer
+
+        For y = 1 To 5
+            TextBox1.Text = TextBox1.Text & "$" & vbCr
+        Next y
+    End Sub
+End Class
